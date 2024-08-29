5abe6d2fb9c24ac8765e415b564ebbf4aa0bd1aa
m999rvi <rudy.vissers@m-team.be>
2024-08-29 09:12:52 +0200

    chapter 1/question 1

---
+++ src/main/java/be/vsolutions/chapter1/question1/Main.java
@@ -0,0 +1,20 @@
+package be.vsolutions.chapter1.question1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /* Write a program that reads an integer and prints it in binary, octal,
+         * and hexadecimal. Print the reciprocal as a hexadecimal floating-point number.
+         */
+        var in = new Scanner(System.in);
+        var i = in.nextInt();
+//        var i = 10;
+        var binaryString = Integer.toBinaryString(i); // no format specifiers found
+        System.out.printf("binary:%s\n", binaryString); // s for string
+        System.out.printf("octal:%o\n", i); // o for octal
+        System.out.printf("hexadecimal:%x\n", i); // x for hexadecimal
+        float f = i;
+        System.out.printf("hexadecimal floating point number:%a\n", f); // a for hexadecimal floating-point
+    }
+}
